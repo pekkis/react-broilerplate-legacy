30a91c8462ce6a7083867bd20ae6ecd54a9742e5
Panu Leppäniemi <me@panuleppaniemi.com>
2015-10-20 01:49:01 -0700

    New components

--- src/client.js
+++ src/client.js
@@ -1,3 +1,36 @@
-require('./client.css');
+import './client.css';
 
-console.log('pekkis sanoo puup!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+});
+
+const HelloWorldApp = React.createClass({
+    render: function() {
+        const names = this.props.names;
+
+
+        return (
+            <div>
+                <h1>Lusso</h1>
+
+                {names.map(name =>
+                    <HelloWorld name={name}/>
+                )}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <HelloWorldApp names={['Tussi', 'Lussi']}/>,
+    document.getElementById('app')
+);
