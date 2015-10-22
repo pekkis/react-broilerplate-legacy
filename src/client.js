47463e68cb3ee83d2405cad3c5838eab3d0bc5f1
Mikko Forsström <pekkisx@gmail.com>
2015-10-22 09:55:46 +0300

    Puutarhanhoito.

--- src/client.js
+++ src/client.js
@@ -1,29 +1,88 @@
 import './client.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import { List } from 'immutable';
+import classnames from 'classnames';
 
-import HelloWorldApp from './components/HelloWorldApp';
-import Index from './components/Index';
-import Greeter from './components/Greeter';
+class TodoApp extends React.Component {
 
-import { Router, Route, Link, IndexRoute } from 'react-router';
+    constructor() {
+        super();
+        this.state = {
+            todos: List.of(
+                {
+                    id: 1,
+                    todo: 'Hanki 100 litraa kuningasvettä',
+                    done: true,
+                },
+                {
+                    id: 2,
+                    todo: 'Osta teollisia puutarhanhoitovälineitä',
+                    done: true,
+                },
+                {
+                    id: 3,
+                    todo: 'Paloittele "liha"',
+                    done: true,
+                },
+                {
+                    id: 4,
+                    todo: 'Kippaa happo Vantaajokeen',
+                    done: false,
+                },
+                {
+                    id: 5,
+                    todo: 'Pese kylppäri HYVIN',
+                    done: false,
+                },
+                {
+                    id: 6,
+                    todo: 'Palauta puutarhanhoitovehkeet 2 vko kuluessa',
+                    done: false,
+                }
+            )
+        }
+    }
 
-import { createHistory } from 'history';
-const history = createHistory();
+    render() {
+        const { todos } = this.state;
+        return (
+            <TodoList todos={todos} />
+        );
+    }
+};
 
-const routes = (
-    <Router history={history}>
+const TodoList = (props) => {
 
-        <Route path="/" component={HelloWorldApp}>
-            <IndexRoute component={Index} />
-            <Route path="/hello/:name" component={Greeter}></Route>
-        </Route>
-    </Router>
-);
+    const { todos } = props;
+
+    return (
+        <ul>
+            {todos.map(todo =>
+                <Todo key={todo.id} todo={todo} />
+            )}
+        </ul>
+    );
+};
+
+const Todo = (props) => {
+
+    const { todo } = props;
+
+    const classes = classnames(
+        'todo',
+        { done: todo.done }
+    );
+
+    return (
+        <li className={classes}>
+            {todo.todo}
+        </li>
+    );
+};
+
 
 ReactDOM.render(
-    routes,
+    <TodoApp />,
     document.getElementById('app')
 );
