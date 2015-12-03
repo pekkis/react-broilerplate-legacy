b28fafd3fdad9aefb793881e43ca9ef044dd76c6
Mikko Forsström <puhemies@diktaattoriporssi.com>
2015-12-03 17:19:20 +0200

    Removed redux stuff.

--- src/client.js
+++ src/client.js
@@ -1,34 +1,3 @@
 import './client.less';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-// Redux or changes caused by Redux
-import store from './store';
-import { Provider } from 'react-redux';
-import TodoApp from "./components/smart/TodoAppContainer";
-
-const app = (
-    <Provider store={store}>
-        <TodoApp />
-    </Provider>
-);
-
-let rootElement = app;
-
-if (__DEVELOPMENT__ && __DEVTOOLS__) {
-
-    const { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
-    rootElement = (
-        <div>
-            {app}
-            <DebugPanel top right bottom>
-                <DevTools store={store} monitor={LogMonitor} />
-            </DebugPanel>
-        </div>
-    );
-}
-
-ReactDOM.render(
-    rootElement,
-    document.getElementById('app')
-);
+console.log('quack says the duck');
